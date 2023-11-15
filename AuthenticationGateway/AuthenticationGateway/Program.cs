ccc6b0dffeea85d14318d44b845e59ad7e02eaa2
sekulas <sekula.sebastian22@gmail.com>
2023-11-15 17:35:00 +0100

    Added WS routes, cors, logging

--- AuthenticationGateway/AuthenticationGateway/Program.cs
+++ AuthenticationGateway/AuthenticationGateway/Program.cs
@@ -1,5 +1,6 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Values;
 
 new WebHostBuilder()
     .UseKestrel()
@@ -13,18 +14,42 @@ new WebHostBuilder()
             .AddJsonFile("ocelot.json")
             .AddEnvironmentVariables();
     })
-    .ConfigureServices(s => {
-        s.AddOcelot();
+    .ConfigureServices(services => {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(
+                "all",
+                policy => policy.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+        });
+        services.AddOcelot();
     })
     .ConfigureLogging((hostingContext, logging) =>
     {
         //add your logging
+        logging.AddConsole();
     })
     .UseIISIntegration()
     .Configure(app =>
     {
+        app.UseCors("all");
         app.UseWebSockets();
-        app.UseOcelot().Wait();
+        app.UseOcelot(new OcelotPipelineConfiguration
+        {
+            PreQueryStringBuilderMiddleware = async (context, next) =>
+            {
+                // Log information on the console before processing the request
+                Console.WriteLine($"Request: {context.Request.Path}");
+                await next.Invoke();
+            },
+            PreAuthenticationMiddleware = async (context, next) =>
+            {
+                // Add any pre-authentication logic here if needed
+                await next.Invoke();
+            }
+            // Add other middleware as needed
+        }).Wait();
     })
     .Build()
     .Run();
